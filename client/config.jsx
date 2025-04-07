e7c56a138fd80b9a266a75c24f366bade661c9e7
vinay-vk-kumar <omrajputkumar@gmail.com>
2025-04-07 22:00:14 +0530

    Resolve merge conflict

--- client/config.jsx
+++ client/config.jsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
+
 export const BACKNED_URL = "https://expirey.onrender.com"
-=======
-export const BACKNED_URL = "https://expirey.onrender.com"
->>>>>>> a18464a (Fix: Downgraded React to 18.x for Vercel compatibility)
+
