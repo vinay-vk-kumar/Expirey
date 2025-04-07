ef4cf415c916cbbb1319549865c291a8aa86a155
vinay-vk-kumar <omrajputkumar@gmail.com>
2025-04-07 21:58:55 +0530

    Fix: Downgraded React to 18.x for Vercel compatibility

--- client/config.jsx
+++ client/config.jsx
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export const BACKNED_URL = "https://expirey.onrender.com"
+=======
+export const BACKNED_URL = "https://expirey.onrender.com"
+>>>>>>> a18464a (Fix: Downgraded React to 18.x for Vercel compatibility)
