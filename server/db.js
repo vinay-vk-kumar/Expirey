728762c7cf933520f2cd05aa07ee30c48896c71a
vinay-vk-kumar <omrajputkumar@gmail.com>
2025-04-08 10:08:31 +0530

    added timestamps for item and user

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true, select: false },
     name: { type: String, required: true }
-});
+},{ timestamps: true });
 
 const expireySchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -13,7 +13,7 @@ const expireySchema = new mongoose.Schema({
     date: { type: String, required: true },
     comment: { type: String },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" } // Reference to User
-});
+},{ timestamps: true });
 
 const User = mongoose.model("User", userSchema);
 const Expirey = mongoose.model("Expirey", expireySchema);
